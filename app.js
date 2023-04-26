5c13c5120b096eb493f9bd2049292ab6c5917308
Shy <hyhy9501@naver.com>
2023-04-27 02:22:40 +0900

    add bootstrap and container

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
-const methodOverride = require('method-override')
-const Noritur = require('./models/noritur')
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const Noritur = require('./models/noritur');
 
 mongoose.connect('mongodb://127.0.0.1:27017/noritur', {
     useNewUrlParser: true,
@@ -17,7 +18,7 @@ db.once("open", () => {
 
 const app = express();
 
-
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
