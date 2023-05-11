433d118458a24a31cf692710f6612cab203c8d09
Shy <hyhy9501@naver.com>
2023-05-11 21:37:12 +0900

    defined comment model

--- models/noritur.js
+++ models/noritur.js
@@ -4,7 +4,13 @@ const Schema = mongoose.Schema;
 const NoriturSchema = new Schema({
     title: String,
     image: String,
-    description: String
+    description: String,
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Noritur', NoriturSchema);
